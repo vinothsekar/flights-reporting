aa906699c3d2a6ef23aad6913990c6a9a23acf3e
vinothsekar <vinothsekark@gmail.com>
2020-09-30 00:33:30 -0400

    modified spark job and added shell script to run spark submit

--- run_flights_trip.sh
+++ run_flights_trip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export SPARK_MAJOR_VERSION=2
 echo " flight trip spark job started "
 spark-submit --master yarn --deploy-mode cluster --num-executors 10 --executor-cores 2 flights_trip.py
 
